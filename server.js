36f3def7ff5917d40b3e7155fa49980f4e04de23
Waldo Mauricio BC <112277562+WalMauBC@users.noreply.github.com>
2024-05-04 23:11:31 -0500

    W01 FIX

--- server.js
+++ server.js
@@ -1,44 +1,45 @@
-/* ******************************************
- * This server.js file is the primary file of the 
- * application. It is used to control the project.
- *******************************************/
-/* ***********************
- * Require Statements
- *************************/
-const express = require("express")
-const expressLayouts = require ("express-ejs-layouts")
-const env = require("dotenv").config()
-const app = express()
-const static = require("./routes/static")
+    /* ******************************************
+    * This server.js file is the primary file of the 
+    * application. It is used to control the project.
+    *******************************************/
+    /* ***********************
+    * Require Statements
+    *************************/
+    const express = require("express")
+    const expressLayouts = require ("express-ejs-layouts")
+    const env = require("dotenv").config()
+    const app = express()
+    const static = require("./routes/static")
 
 
-/* ***********************
- * View Engine Templates
- *************************/
-app.set("view engine", "ejs")
-app.use(expressLayouts)
-app.set("layout", "./layouts/layout") // not at views root
+    /* ***********************
+    * View Engine Templates
+    *************************/
+    app.set("view engine", "ejs")
+    app.use(express.static("public"))
+    app.set("layout", "./layouts/layout") // not at views root
+    app.set("patials", "./partials/navigation")
 
-/* ***********************
- * Routes*************************/
-app.use(require("./routes/static"))
-// Index route
-app.get("/", function(req, res){
-  res.render("index", {title: "home"})
-})
- 
-app.use(static)
+    /* ***********************
+    * Routes*************************/
+    app.use(require("./routes/static"))
+    // Index route
+    app.get("/", function(req, res){
+      res.render("index", {title: "home"})
+    })
+    
 
-/* ***********************
- * Local Server Information
- * Values from .env (environment) file
- *************************/
-const port = process.env.PORT
-const host = process.env.HOST
 
-/* ***********************
- * Log statement to confirm server operation
- *************************/
-app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
-})
+    /* ***********************
+    * Local Server Information
+    * Values from .env (environment) file
+    *************************/
+    const port = process.env.PORT
+    const host = process.env.HOST
+
+    /* ***********************
+    * Log statement to confirm server operation
+    *************************/
+    app.listen(port, () => {
+      console.log(`app listening on ${host}:${port}`)
+    })
